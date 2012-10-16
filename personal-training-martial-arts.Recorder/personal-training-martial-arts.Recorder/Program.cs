c59a388f2fdac988b65c77bdeb39e9a6ae739bab
Pablo Calleja <pablocallejaibanez@gmail.com>
2012-10-16 16:34:20 +0200

    Crea el proyecto con su content del Recorder

---
+++ personal-training-martial-arts.Recorder/personal-training-martial-arts.Recorder/Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace personal_training_martial_arts.Recorder
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (Game1 game = new Game1())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
+
